810aee732eb3ef6aa7d3c8f0ab740be0d8445d7f
crclayton <charlesrussellclayton@gmail.com>
2016-01-03 22:44:10 -0800

    CONCEPT AND PLAN

---
+++ notes-flashback/notes_flashback.py
@@ -0,0 +1,19 @@
+# notes-flashback
+
+# This program sends an email to me once a day containing a
+# page from my old notes. It creates a flashback, reminding
+# me of a lecture/concept that I might have forgotten about
+# without the review. Alternatively, if I look at the notes
+# and have no recollection of them, it'll signal to me that
+# I need to review a topic.
+
+# 03-Jan-16
+
+"""
+Create task scheduler to run this script once a day.
+
+1. Grab notes from OneNote document on OneDrive
+2. Take random page (perhaps by converting to PDF)
+3. Email random page to self
+"""
+
