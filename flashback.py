610f931f06d8dbeec4191d37b679e29b8bad0728
crclayton <charlesrussellclayton@gmail.com>
2016-02-07 20:24:20 -0800

    Moved to Ubuntu, added random pdf, improved arguments

--- flashback.py
+++ flashback.py
@@ -8,18 +8,24 @@
 # I need to review a topic.
 
 # To use: create a task scheduler event to call the following: 
-# > python notes_flashback.py <your email> <your password> <your notes pdf>
+# > python notes_flashback.py --outlook <your outlook> -- password <your password> -- directory <your notes directory>
 
-# 03-Jan-16
+# I use outlook because my gmail uses two-step verification, 
+# and I wasn't bothered getting around that
 
+# 07-Feb-16
 
 
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from random import randint
 
-import smtplib, sys
+import smtplib, sys, argparse, os, random
 import email.mime.multipart as multipart, email.mime.application as application
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--outlook")
+parser.add_argument("--password")
+parser.add_argument("--dir")
 
 def get_random_pdf_page(input_pdf_path, output_pdf_path):
     pdf_file = PdfFileReader(input_pdf_path)
@@ -41,10 +47,10 @@ def create_pdf_attachment(filename):
     attachment.add_header("Content-Disposition", "attachement", filename=filename)
     return attachment
 
-def send_gmail(recipient, sender, password, attachment):
+def send_outlook(recipient, sender, password, attachment):
 
     mail = multipart.MIMEMultipart()
-    mail["Subject"] = "Notes Flashback"
+    mail["Subject"] = "Remember this?"
     mail["From"] = sender
     mail["To"] = recipient
     mail.attach( create_pdf_attachment(attachment) )
@@ -55,16 +61,18 @@ def send_gmail(recipient, sender, password, attachment):
     server.login(sender, password)
     server.sendmail(sender,[recipient], mail.as_string())
     server.close()
- 
-def main(email, password, pdf):
-    print("opening pdf")
-    get_random_pdf_page(pdf, "temp.pdf")
-
-    print("sending email")
-    send_gmail(email, email, password, "temp.pdf")
-
-    print("email sent")
 
 if __name__ == "__main__":
-    print(sys.argv)
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    arguments = parser.parse_args()
+
+    print("selecting random pdf")	
+    pdf = arguments.dir + random.choice(os.listdir(arguments.dir))
+	
+    print("selecting random page")
+    temp_pdf = arguments.dir + "temp.pdf"
+    get_random_pdf_page(pdf, temp_pdf)
+
+    print("sending email")
+    send_outlook(arguments.outlook, arguments.outlook, arguments.password, temp_pdf)
+
+    print("email sent")
