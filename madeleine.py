d0695de415a8ae72347173748ee8508ad7dc0ab4
crclayton <charlesrussellclayton@gmail.com>
2017-01-05 18:43:32 -0800

    Updated script

--- madeleine.py
+++ madeleine.py
@@ -1,21 +1,3 @@
-# notes-flashback
-
-# This program sends an email to me once a day containing a
-# page from my old notes. It creates a flashback, reminding
-# me of a lecture/concept that I might have forgotten about
-# without the review. Alternatively, if I look at the notes
-# and have no recollection of them, it'll signal to me that
-# I need to review a topic.
-
-# To use: create a task scheduler event to call the following: 
-# > python notes_flashback.py --outlook <your outlook> -- password <your password> -- directory <your notes directory>
-
-# I use outlook because my gmail uses two-step verification, 
-# and I wasn't bothered getting around that
-
-# 07-Feb-16
-
-
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from random import randint
 
@@ -23,9 +5,19 @@ import smtplib, sys, argparse, os, random
 import email.mime.multipart as multipart, email.mime.application as application
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--outlook")
-parser.add_argument("--password")
-parser.add_argument("--dir")
+parser.add_argument("email")
+parser.add_argument("password")
+parser.add_argument("dir")
+
+
+def get_random_file(dir, type):
+    files = [os.path.join(path, filename)
+         for path, dirs, files in os.walk(dir)
+         for filename in files
+         if filename.endswith(type)]
+
+    return random.choice(files)
+
 
 def get_random_pdf_page(input_pdf_path, output_pdf_path):
     pdf_file = PdfFileReader(input_pdf_path)
@@ -40,6 +32,7 @@ def get_random_pdf_page(input_pdf_path, output_pdf_path):
     page_to_email.write(file_to_email)
     file_to_email.close()
 
+
 def create_pdf_attachment(filename):
     file = open(filename, "rb")
     attachment = application.MIMEApplication(file.read(),_subtype="pdf")
@@ -47,32 +40,36 @@ def create_pdf_attachment(filename):
     attachment.add_header("Content-Disposition", "attachement", filename=filename)
     return attachment
 
-def send_outlook(recipient, sender, password, attachment):
 
+def send_email(recipient, sender, password, attachment="", host="smtp.gmail.com"):
     mail = multipart.MIMEMultipart()
     mail["Subject"] = "Remember this?"
     mail["From"] = sender
     mail["To"] = recipient
-    mail.attach( create_pdf_attachment(attachment) )
+    if attachment != "": mail.attach(create_pdf_attachment(attachment))
 
-    server = smtplib.SMTP('smtp.live.com', 587)
+    server = smtplib.SMTP(host, 587)
     server.ehlo()
     server.starttls()
+    server.ehlo()
     server.login(sender, password)
     server.sendmail(sender,[recipient], mail.as_string())
-    server.close()
+    server.quit()
+
 
 if __name__ == "__main__":
     arguments = parser.parse_args()
 
     print("selecting random pdf")	
-    pdf = arguments.dir + random.choice(os.listdir(arguments.dir))
-	
+    pdf = get_random_file(arguments.dir, ".pdf")
+
     print("selecting random page")
-    temp_pdf = arguments.dir + "temp.pdf"
+    temp_pdf = os.getcwd() + "/temp.pdf"
+    print(temp_pdf)
+
     get_random_pdf_page(pdf, temp_pdf)
 
     print("sending email")
-    send_outlook(arguments.outlook, arguments.outlook, arguments.password, temp_pdf)
+    send_email(arguments.email, arguments.email, arguments.password, temp_pdf)
 
     print("email sent")
